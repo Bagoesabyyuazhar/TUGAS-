e349122b554f2c7273b7b67dc2fc39900cc126d2
Bagoesabyyuazhar <104241619+Bagoesabyyuazhar@users.noreply.github.com>
2022-04-23 11:44:48 +0700

    Add files via upload

---
+++ @startuml if (BAGUS.cpp
@@ -0,0 +1,11 @@
+@startuml if 
+
+   start
+   if(int a lebih besar dari) the (yes)
+   #LightSkyBlue:cout << a lebih besar dari b;
+else (no)
+   #LightSkyBlue:cout << b lebih besar dari a;
+endif
+stop
+
+@enduml
